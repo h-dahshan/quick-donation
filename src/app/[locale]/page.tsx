f7bd64653bb75c1a29fe3f6b46ca4cb3073875e4
Hossam Dahshan <hossam.dahshan@wscdev.net>
2024-11-30 15:58:48 +0300

    init: installed shadcn ui

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,7 +12,9 @@ export default function DonationPage() {
         {t("hello")}
       </main>
 
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        Write Footer Here
+      </footer>
     </div>
   );
 }
