51c94b44a91b7ff488b559eaedabfb465d45b86c
Hossam Dahshan <hossam.dahshan@wscdev.net>
2024-12-01 19:12:15 +0300

    fix: stripe PRD issue

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  typescript: true,
-  apiVersion: "2024-11-20.acacia",
-});
-
 export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    typescript: true,
+    apiVersion: "2024-11-20.acacia",
+  });
+
   const { data } = await req.json();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { customer, amount } = data;
