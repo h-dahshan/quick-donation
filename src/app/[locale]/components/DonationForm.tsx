5765d51c84817ed2e36304ca39eb6fba7be41b8e
Hossam Dahshan <hossam.dahshan@wscdev.net>
2024-12-02 20:36:46 +0300

    test: roleback to old stripe version

--- src/app/[locale]/components/DonationForm.tsx
+++ src/app/[locale]/components/DonationForm.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useState } from "react";
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 
 import {
   Form,
@@ -37,10 +35,9 @@ import {
   useStripe,
 } from "@stripe/react-stripe-js";
 
-import { cn } from "@/lib/utils";
-
 export default function DonationForm() {
   const t = useTranslations("donationPage");
+  const locale = useLocale();
 
   const form = useForm({
     resolver: zodResolver(
@@ -69,7 +66,6 @@ export default function DonationForm() {
 
   const stripe = useStripe();
   const elements = useElements();
-  const [intentDone, setIntentDone] = useState(false);
 
   async function onSubmit(values: {
     name: string;
@@ -88,22 +84,20 @@ export default function DonationForm() {
     try {
       if (!stripe || !elements) return null;
 
-      elements.submit();
-
       // call server action to make a payment intent
       const { data } = await axios.post("/api/create-payment-intent", {
         data: postReqPayload,
       });
 
+      elements.submit();
+
       await stripe?.confirmPayment({
         elements,
         clientSecret: data,
         confirmParams: {
-          return_url: `${window.origin}/success`,
+          return_url: `${window.origin}/${locale}/feedback`,
         },
       });
-
-      setIntentDone(true);
     } catch (error) {
       console.log(error);
     }
@@ -112,167 +106,157 @@ export default function DonationForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
-        {!intentDone && (
-          <>
-            <FormSection label={t("donationAmount")}>
-              <p>
-                {t("itemCost")}{" "}
-                <span className="text-green-600">{"€100.00"}</span>
-              </p>
+        <FormSection label={t("donationAmount")}>
+          <p>
+            {t("itemCost")} <span className="text-green-600">{"€100.00"}</span>
+          </p>
 
-              <FormField
-                control={form.control}
-                name="quantity"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("quantity")}</FormLabel>
+          <FormField
+            control={form.control}
+            name="quantity"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("quantity")}</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
                     <FormControl>
-                      <Select
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select a verified email to display" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                          {Array.from({ length: 10 }).map((_, idx) => (
-                            <SelectItem key={idx} value={`${idx + 1}`}>
-                              {idx + 1}
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
                     </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </FormSection>
+                    <SelectContent>
+                      {Array.from({ length: 10 }).map((_, idx) => (
+                        <SelectItem key={idx} value={`${idx + 1}`}>
+                          {idx + 1}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSection>
 
-            <Separator className="my-6" />
+        <Separator className="my-6" />
 
-            <FormSection label={t("personalInfo")}>
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("name")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("email")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="mobile"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("mobile")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="street"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("street")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="city"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("city")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="country"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel required>{t("country")}</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </FormSection>
+        <FormSection label={t("personalInfo")}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("name")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("email")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="mobile"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("mobile")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="street"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("street")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("city")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel required>{t("country")}</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSection>
 
-            <Separator className="my-6" />
+        <Separator className="my-6" />
 
-            <FormSection label={t("feeCoverage")}>
-              <FormField
-                control={form.control}
-                name="coverFee"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <div className="flex items-center gap-2">
-                        <Checkbox
-                          id="fee"
-                          value={field.value.toString()}
-                          onCheckedChange={field.onChange}
-                        />
-                        <label
-                          htmlFor="fee"
-                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                          {t("feeCoverageAck")}
-                        </label>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </FormSection>
+        <FormSection label={t("feeCoverage")}>
+          <FormField
+            control={form.control}
+            name="coverFee"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex items-center gap-2">
+                    <Checkbox
+                      id="fee"
+                      value={field.value.toString()}
+                      onCheckedChange={field.onChange}
+                    />
+                    <label
+                      htmlFor="fee"
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {t("feeCoverageAck")}
+                    </label>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </FormSection>
 
-            <Separator className="my-6" />
-          </>
-        )}
+        <Separator className="my-6" />
 
-        <div
-          className={cn([
-            "flex flex-col justify-between align-bottom gap-2 md:flex-row",
-            intentDone && "md:flex-col",
-          ])}
-        >
+        <div className="flex flex-col justify-between gap-4">
           <p>
             {t("totalDonation")}{" "}
             <span className="text-green-600">
@@ -281,17 +265,11 @@ export default function DonationForm() {
           </p>
 
           {/* STIPE */}
-          <div
-            className={cn([
-              "flex flex-col",
-              !intentDone && "invisible max-w-0 max-h-0",
-            ])}
-          >
-            <PaymentElement options={{ layout: { type: "accordion" } }} />
-            <Button onClick={() => elements?.submit()}>{t("pay")}</Button>
+          <div className="flex flex-col">
+            <PaymentElement options={{ layout: { type: "auto" } }} />
           </div>
 
-          {!intentDone && <Button type="submit">{t("checkout")}</Button>}
+          <Button type="submit">{t("pay")}</Button>
         </div>
       </form>
     </Form>
