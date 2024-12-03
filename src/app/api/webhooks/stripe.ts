8aea1b1395408f7fffdce3370690ff7d3944ec38
Hossam Dahshan <hossam.dahshan@wscdev.net>
2024-12-03 19:20:51 +0300

    feat: added CORS to stipe hook handler

--- src/app/api/webhooks/stripe.ts
+++ src/app/api/webhooks/stripe.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import Stripe from "stripe";
+import Cors from "cors";
 
+import Stripe from "stripe";
 import prisma from "@/lib/prisma";
 
 const webhookSercret = process.env.STRIPE_WEBHOOK_SECRET!;
@@ -8,6 +9,20 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
   apiVersion: "2024-11-20.acacia",
 });
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise((resolve, reject) => {
+    Cors({ methods: ["POST", "GET", "HEAD"] })(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export const config = {
   api: {
@@ -19,6 +34,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Run the middleware
+  await runMiddleware(req, res);
+
   if (req.method === "POST") {
     const sig = req.headers["stripe-signature"];
     if (!sig) return;
@@ -30,6 +48,8 @@ export default async function handler(
       console.error("Error verifying webhook signature:", err);
       return res.status(400).send(`Webhook Error: ${err}`);
     }
+
+    console.log(event);
 
     // Handle the event
     if (event.type === "payment_intent.succeeded") {
@@ -43,6 +63,8 @@ export default async function handler(
           paid: true,
         },
       });
+
+      console.log("FINISHED UPDATE");
     } else {
       console.log(`Unhandled event type: ${event.type}`);
     }
